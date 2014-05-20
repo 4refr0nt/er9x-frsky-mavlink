6f17dff8f05d5a53f32d08e62cb9601f3c1d68cd
4refr0nt@gmail.com <4refr0nt@gmail.com@9c7155e4-4ac9-12ad-9ab2-f90c48cc6b03>
2014-05-20 16:30:15 +0000

    Fixes A1/A2 display bug by Mike.

--- trunk/source/er9x-frsky/stamp-er9x.h
+++ trunk/source/er9x-frsky/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "15.05.2014"
-#define TIME_STR "23:43:51"
-#define SUB_VERS 123-
+#define DATE_STR "20.05.2014"
+#define TIME_STR "21:08:29"
+#define SUB_VERS 124-
 #define SVN_VERS "er9x-r1"
 #define MOD_VERS "FRSKY   128  "
